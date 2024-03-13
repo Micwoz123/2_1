885edae638b4ee18acc4e80f68049189f723183e
Micwoz123 <161605924+Micwoz123@users.noreply.github.com>
2024-03-13 13:23:33 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String[] args) {
+  String ciag="Hello world";
+
+    for(int i=0; i<ciag.length(); i++) {
+      System.out.println(ciag.charAt(i));
+    }
+
+    
+  }
+
+ 
+}
